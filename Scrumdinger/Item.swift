27e0c22304e1848143d745cedda56403c5770750
Patrick <patrickpc1015@gmail.com>
2024-10-23 15:58:43 -0700

    Initial Commit

---
+++ Scrumdinger/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Scrumdinger
+//
+//  Created by Patrick on 10/23/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
